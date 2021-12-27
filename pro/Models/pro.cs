cea3e570e3422517ed19ab8bdaadcdba2459edba
Priyanka_Mukhi <priyankamukhi96@gmail.com>
2021-12-27 11:34:04 +0530

    files added

---
+++ pro/Models/pro.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pro.Models
+{
+    public class pro
+    {
+    }
+}
---
+++ pro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pro.Startup))]
+namespace pro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
