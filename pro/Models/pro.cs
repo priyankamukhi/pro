00244cb860d348cb03321d74f18ef46f6b4b19a9
Priyanka_Mukhi <priyankamukhi96@gmail.com>
2021-12-28 10:28:50 +0530

    files modified

--- pro/Models/pro.cs
+++ pro/Models/pro.cs
@@ -7,5 +7,11 @@ namespace pro.Models
 {
     public class pro
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Class { get; set; }
+        public int rollno { get; set; }
+        public string emailid { get; set; }
+
     }
 }
